c74c381efd8e0d96299678c55c25550b900ceabf
Ajith-Kanagasabai <ajithkanagasabai17@gmail.com>
2024-09-03 18:28:20 +0530

    first commit

--- src/components/Login/Firebase/Firebase.config.js
+++ src/components/Login/Firebase/Firebase.config.js
@@ -1,12 +1,18 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDb43o5H-G9RHAHOE5SOxJ3BqKil71B8p8",
+  authDomain: "sample-68117.firebaseapp.com",
+  projectId: "sample-68117",
+  storageBucket: "sample-68117.appspot.com",
+  messagingSenderId: "432893177065",
+  appId: "1:432893177065:web:2cc7bf74bcc1bc72289478"
 };
 
 export default firebaseConfig;
